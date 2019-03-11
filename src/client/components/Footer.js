2c95d51d6edb8089180bad6c99f4d0cfdf102a2b
ilker ALTIN <ilkerobi@gmail.com>
2019-03-11 21:56:11 +0100

    Update readme, footer link

--- src/client/components/Footer.js
+++ src/client/components/Footer.js
@@ -6,7 +6,7 @@ export default () => {
       <div className="footer-copyright">
         <div className="container">
           © 2019 SSR Daily News - ilker ALTIN
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a className="grey-text text-lighten-4 right" href="https://github.com/ilkeraltin/react-ssr-news">
             Source Code
           </a>
         </div>
